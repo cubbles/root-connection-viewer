80897616941544cd4a07a904db08456c603ede84
Edwin Gamboa <edwjov92@gmail.com>
2017-07-19 17:25:22 -0500

    CUBBLES-70: Fix initial heights

--- webpackages/root-connection-viewer/root-connection-viewer/jsFunctions.js
+++ webpackages/root-connection-viewer/root-connection-viewer/jsFunctions.js
@@ -11,10 +11,16 @@
     openViewerModal: function (definitions, next) {
       var viewerModal = $('#iframe_view_modal');
       viewerModal.on('shown.bs.modal', function (e) {
+        document.querySelector('cubx-iframe-viewer').setHeight('95%');
+        document.querySelector('cubx-iframe-viewer').setSlotChange({slot: 'viewerHeight', value: '80%'});
         document.querySelector('cubx-iframe-viewer').setSlotChange({slot: 'scale', value: 'auto'});
       });
       viewerModal.modal('show');
       next({slot: 'definitions', value: definitions});
     }
   };
+
+  document.addEventListener('cifReady', function () {
+    document.querySelector('.modal-body').style.height = window.innerHeight * 0.7 + 'px';
+  });
 })();
